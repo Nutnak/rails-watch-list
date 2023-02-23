f36c8fe1f367b5d2c423885f5760c90456bf6da5
Quentin Doulcet <quentinucme@hotmail.fr>
2023-02-23 16:07:21 -0500

    almost all the crud

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,13 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @lists = List.all
+  end
+
+  def new
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/lists', to: 'list#index'
-  get '/lists/:id', to: 'list#show'
-  get '/lists/new', to: 
+  get '/lists', to: 'lists#index'
+  get '/lists/new', to: 'lists#new'
+  get '/lists/:id', to: 'lists#show'
 # post "lists"
 end
