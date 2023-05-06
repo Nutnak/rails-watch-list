9642e5d8fcc986a9c831f31c999a3a2a1f8dd4be
Quentin Doulcet <quentinucme@hotmail.fr>
2023-05-06 13:49:36 -0400

    je fixe github

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/lists', to: 'lists#index'
-  get '/lists/new', to: 'lists#new'
-  get '/lists/:id', to: 'lists#show', as: :list
-  post 'lists', to: 'lists#create'
-# post "lists"
+  # get '/lists', to: 'lists#index'
+  # get '/lists/new', to: 'lists#new'
+  # get '/lists/:id', to: 'lists#show', as: :list
+  # post 'lists', to: 'lists#create'
+  resources :lists, only: [:index, :new, :show, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
 end
